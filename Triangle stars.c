065d02404a2a228b0dec771cd42ea7311abba975
SatyaVaraha <125621469+SatyaVaraha@users.noreply.github.com>
2023-03-01 09:23:18 +0530

    Printing stars

---
+++ Triangle stars.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main() {
+    int i,j,k,n;
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n-i; j++)
+            printf(" ");
+        for(k=1; k<=i; k++)
+            printf(" *");
+        printf("\n");
+    }
+
+}
